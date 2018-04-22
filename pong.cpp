501ba9aa375943a66e0f3b6d02e02b03f34c0acc
Pranav Bhatnagar <unknownblueguy6@gmail.com>
2018-04-22 19:14:13 +0530

    Fix movement, Add mem funcs to Particle class

--- pong.cpp
+++ pong.cpp
@@ -8,12 +8,47 @@
 #include <string.h>
 #include "timer.h"
 
-class Particle{
-	unsigned vx; //velocity along x-axis
-	unsigned vy; //velocity along y-axis
-	unsigned x; //x coordinate
-	unsigned y; //y coordinate
+enum Axes
+{
+	X,
+	Y
 };
+
+class Particle
+{
+	public:
+		void move();
+		void changeVel(bool choice, int v);
+		unsigned getPos(bool choice);
+		
+		int vx; //velocity along x-axis
+		int vy; //velocity along y-axis
+		unsigned x; //x coordinate
+		unsigned y; //y coordinate
+};
+
+void Particle :: move(){
+	x += vx;
+	y += vy;
+}
+
+void Particle :: changeVel(bool choice, int v){
+	if (!choice){
+		vx = v;
+	}
+	else{
+		vy = v;
+	}
+}
+
+unsigned Particle :: getPos(bool choice){
+	if (!choice){
+		return x;
+	}
+	else{
+		return y;
+	}
+}
 
 const unsigned SCREEN_WIDTH = 640;
 const unsigned SCREEN_HEIGHT = 480;
@@ -36,7 +71,7 @@ void close(){
 	SDL_DestroyRenderer(gRenderer);
 	SDL_DestroyWindow(gWindow);
 	gWindow = 0;
-	gRenderer = 0;z`
+	gRenderer = 0;
 	SDL_Quit();
 }
 
@@ -51,37 +86,47 @@ int main(int argc, char* argv[]){
 
 	LTimer capTimer;
 
-	int x = 10, y = 40;
+	Particle p1 = {0,0, 20, 30}, p2 = {0, 0, 50, 30};
+
 
 	while(!quit){
-
+		
+		capTimer.start();
+		
 		while(SDL_PollEvent(&e) != 0){
 			
 			if( e.type == SDL_QUIT ){
 				quit = true;
 			}
+		}
 
-			else if (e.type == SDL_KEYDOWN){
+		const Uint8* keyStates = SDL_GetKeyboardState( NULL );
 
-				switch (e.key.keysym.sym){
-					case SDLK_UP: y -= 10;
-					break; 
-					case SDLK_DOWN: y += 10;
-					break; 
-					case SDLK_LEFT: x -= 10;
-					break; 
-					case SDLK_RIGHT: x += 10;
-					break; 
-				}
-			}
+		if(keyStates[SDL_SCANCODE_UP]){
+			p2.changeVel(Y, -10);
+			p2.move();
+		}
+		if(keyStates[SDL_SCANCODE_DOWN]){
+			p2.changeVel(Y, 10);
+			p2.move();
+		}
+		if(keyStates[SDL_SCANCODE_W]){
+			p1.changeVel(Y, -10);
+			p1.move();
+		}
+		if(keyStates[SDL_SCANCODE_S]){
+			p1.changeVel(Y, 10);
+			p1.move();
 		}
 
 		SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0x00, 0x00 );
 		SDL_RenderClear( gRenderer );
 
-		SDL_Rect fillRect = { x, y, SCREEN_WIDTH / 30, SCREEN_HEIGHT / 5};
+		SDL_Rect fillRect = { p1.x, p1.y, SCREEN_WIDTH / 30, SCREEN_HEIGHT / 5};
+		SDL_Rect part = {p2.getPos(X), p2.getPos(Y), SCREEN_WIDTH / 30, SCREEN_HEIGHT / 5};
 		SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );		
 		SDL_RenderFillRect( gRenderer, &fillRect );
+		SDL_RenderFillRect(gRenderer, &part);
 
 		SDL_RenderPresent( gRenderer );
 
